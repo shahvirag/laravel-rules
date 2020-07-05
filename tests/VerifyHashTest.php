d742f05235deef3bd9433834bafdc6a136d81f56
Virag Shah <shahvirag07@gmail.com>
2020-07-05 11:04:38 +0530

    add test for verify hash

--- tests/VerifyHashTest.php
+++ tests/VerifyHashTest.php
@@ -1,12 +1,32 @@
 <?php
 
-use PHPUnit\Framework\TestCase;
+use Illuminate\Support\Facades\Hash;
+use Orchestra\Testbench\TestCase;
+use Shahvirag\LaravelRules\VerifyHash;
 
 class VerifyHashTest extends TestCase
 {
-    /** @test */
-    public function validate_hash()
+    /**
+     * Setup the test environment.
+     */
+    protected function setUp(): void
     {
-        $this->assertTrue(true);
+        parent::setUp();
+    }
+
+    /** @test */
+    public function valid_hash()
+    {
+        $hash = Hash::make('password');
+        $rule = new VerifyHash($hash);
+        $this->assertTrue($rule->passes('password', 'password'));
+    }
+
+    /** @test */
+    public function invalid_hash()
+    {
+        $hash = Hash::make('password');
+        $rule = new VerifyHash($hash);
+        $this->assertFalse($rule->passes('password', 'secret'));
     }
 }
